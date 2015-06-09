6b5efa012fa9fec5f5a0a3f7e0463b6ac3c52865
eloy <eloy.colell@gmail.com>
2015-06-09 00:40:12 -0300

    It recover the secondary structure from the server and generate a dictionary.

--- amphipathic/core.py
+++ amphipathic/core.py
@@ -1,11 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
 class Sequence(object):
 
     def __init__(self, string):
         self.primary = string
+
+    def resume_secondary(self):
+        if not hasattr(self, 'structure'):
+            self.secondary_structure()
+            self.structure = []
+            elements = map(lambda m: m.group(0),
+                           re.finditer(r"(\w)\1*", self.secondary))
+            idx = 0
+            for e in elements:
+                s = {'type': e[0],
+                     'begin': idx,
+                     'end': idx + len(e),
+                     'seq': self.primary[idx:idx + len(e)]}
+                self.structure.append(s)
+                idx += len(e)
+        return self.structure
 
     def obtain_secondary(self):
         # It uses the GOR4 service to estimate the secondary structure.
@@ -17,11 +34,12 @@ class Sequence(object):
         html = BeautifulSoup(requests.post(
             'https://npsa-prabi.ibcp.fr/cgi-bin/secpred_gor4.pl',
             data).text)
-        return ''.join(map(lambda f: f.text, html.select('code font')))
+        self.secondary = ''.join(map(lambda f: f.text,
+                                     html.select('code font')))
 
     def secondary_structure(self):
         if not hasattr(self, 'secondary'):
-            self.secondary = self.obtain_secondary()
+            self.obtain_secondary()
         return self.secondary
 
 
@@ -36,4 +54,5 @@ class Amphipathic(object):
 
 def index(amino_sequence):
     seq = Sequence(amino_sequence)
-    return seq.secondary_structure()
+    print seq.secondary_structure()
+    return seq.resume_secondary()
