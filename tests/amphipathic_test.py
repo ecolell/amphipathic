510f729e5c4759311e32da34d8e9bee36b01e77b
eloy <eloy.colell@gmail.com>
2015-06-20 21:43:45 -0300

    It append some nucleotides to the tests.

--- tests/amphipathic_test.py
+++ tests/amphipathic_test.py
@@ -10,10 +10,28 @@ class TestNoaaclass(unittest.TestCase):
             ('MALPVTALLLPLALLLHAARPSQFRVSPLDRTWNLGETVELKCQVLLSNPTSGCSWLFQPRGA'
              'AASPTFLLYLSQNKPKAAEGLDTQRFSGKRLGDTFVLTLSDFRRENEGCYFCSALSNSIMYFS'
              'HFVPVFLPAKPTTTPAPRPPTPAPTIASQPLSLRPEACRPAAGGAVHTRGLDFACDIYIWAPL'
-             'AGTCGVLLLSLVITLYCNHRNRRRVCKCPRPVVKSGDKPSLSARYV')
+             'AGTCGVLLLSLVITLYCNHRNRRRVCKCPRPVVKSGDKPSLSARYV'),
+            ('ggctgacggctgggttaagaagtttactgaggcagttaacgcctttaagggtttggactggatc'
+             'cgctgccaagttgtccaagttccttgattggatcaaatccaagatcatcccggagctcagagag'
+             'agagcggagtttgttaagaatcttaggcagcttcctcttctcgaggcccagatcaccactttgg'
+             'agcactccaaccctaatcaggagacccaagaacagcttttctcgaacgtccaatacctggcaca'
+             'ccactgtaggaagaacgctccgctctatgcagcggaagcccggagagttttcgcactagagaaa'
+             'cgcgtccttggagcaatgcagttcaagaccaagaatcgaattgaacctgtttgctgtttgatcc'
+             'atggaacgccgggcactggtaaatcacttgccacgactattattggcaggaagatcgctgagta'
+             'cgagaatagcggggtctatagtttgccacctgacccagaccactttgatggctacgccgagcag'
+             'gcagttgtaattatggatgatctgcatcagaacccagacggcaaagatatgagcttgttttgtc'
+             'agatggtttccaccacccccttcgtggttccgatggctgctctcgaggataaaggtagactttt'
+             'cacctccaaatatgtgctggcctcaacaaatgccaaccatatccatccagttacagtcgccgat'
+             'ggaaaggcccttcagcgccgcttccacttcgacacggacattgaattgatggatggcttcgtga'
+             'aaaacgggaaactagatattcagagggcaaccgaggcatgtgaagactgttctccgatcaactt'
+             'tcagaagtgcatgcctctcatttgtggtaaggctctccagctccgtagcaagaagggtgatggc'
+             'atgagatacagcattgataccatgatcacagagatgcgcagggagtcagcccgacgctataata'
+             'ttgggaatgttatagaggcactcttccaa')
         ]
 
     def test_amphipathic_index(self):
-        for seq in self.sequences:
+        for seq in self.sequences[0:1]:
+            print "---"
             resume = amphipathic.index(seq)
+            print resume
             # TODO: Should assert Equals to a list of results
