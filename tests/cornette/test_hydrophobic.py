352031795211bdc7c2c26d7bf0ffd4b9a44404b0
eloy <eloy.colell@gmail.com>
2020-11-03 10:56:55 -0300

    Test cornette-properties' function.

--- tests/cornette/test_hydrophobic.py
+++ tests/cornette/test_hydrophobic.py
@@ -16,7 +16,7 @@ def test_get_scales():
     assert cornette.get_scales() == EXPECTED_SCALES
 
 
-EXPECTED_VALUES = [
+EXPECTED_CLEANED_VALUES = [
     ("1.1", 1.1),
     ("-2.1", -2.1),
     ("N", "N"),
@@ -25,6 +25,57 @@ EXPECTED_VALUES = [
     ("invalid", "invalid"),
 ]
 
-@pytest.mark.parametrize("value,expected", EXPECTED_VALUES)
+
+@pytest.mark.parametrize("value,expected", EXPECTED_CLEANED_VALUES)
 def test_clean(value, expected):
     assert cornette.clean(value) == expected
+
+
+EXPECTED_PROPERTIES_VALUES = [
+    (
+        cornette.is_experimental,
+        True,
+        "prift",
+        False
+    ),
+    (
+        cornette.is_experimental,
+        False,
+        "prift",
+        False
+    ),
+    (
+        cornette.is_owned,
+        True,
+        "prift",
+        True
+    ),
+    (
+        cornette.is_average,
+        True,
+        "prift",
+        False
+    ),
+    (
+        cornette.is_statistical,
+        True,
+        "prift",
+        True
+    ),
+    (
+        cornette.is_statistical,
+        True,
+        "ponnu",
+        True
+    )
+]
+
+
+@pytest.mark.parametrize("property,normalized,scale,expected", EXPECTED_PROPERTIES_VALUES)
+def test_properties(property,normalized,scale,expected):
+    assert property(normalized=normalized, scale=scale) == expected
+
+
+def test_load_table_unknown_scale():
+    with pytest.raises(cornette.UnknownScaleError):
+        cornette.load_table(normalized=True, scale="invalid_one")
